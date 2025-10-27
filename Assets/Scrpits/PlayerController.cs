d48d4b75f3c1945da4d1a1d9b49b7ea5138e64e0
arlequiiin <arlequin@3474.ru>
2025-10-27 23:00:52 +0500

    Добавил объект персонажа, player action map и передвижение персонажа

---
+++ Assets/Scrpits/PlayerController.cs
@@ -0,0 +1,74 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour {
+    private PlayerInputActions input;
+    private Rigidbody2D rb;
+
+    [Header("Движение")]
+    public float moveSpeed = 5f;
+    private Vector2 moveInput;
+
+    [Header("Уворот")]
+    public float dodgeForce = 8f;
+    public float dodgeCooldown = 1f;
+    private bool canDodge = true;
+
+    [Header("Атака")]
+    public GameObject projectilePrefab; // Префаб пули или ближней атаки
+    public float attackCooldown = 0.5f;
+    private bool canAttack = true;
+
+    private void Awake() {
+        input = new PlayerInputActions();
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnEnable() {
+        input.Player.Enable();
+
+        input.Player.Move.performed += ctx => moveInput = ctx.ReadValue<Vector2>();
+        input.Player.Move.canceled += ctx => moveInput = Vector2.zero;
+
+        input.Player.Dodge.performed += _ => TryDodge();
+        input.Player.Attack.performed += _ => TryAttack();
+    }
+
+    private void OnDisable() {
+        input.Player.Disable();
+    }
+
+    private void FixedUpdate() {
+        rb.linearVelocity = moveInput * moveSpeed;
+    }
+
+    private void TryDodge() {
+        if (!canDodge || moveInput == Vector2.zero)
+            return;
+
+        canDodge = false;
+        rb.AddForce(moveInput.normalized * dodgeForce, ForceMode2D.Impulse);
+        Invoke(nameof(ResetDodge), dodgeCooldown);
+    }
+
+    private void ResetDodge() => canDodge = true;
+
+    private void TryAttack() {
+        if (!canAttack)
+            return;
+
+        canAttack = false;
+
+        if (projectilePrefab != null) {
+            GameObject proj = Instantiate(projectilePrefab, transform.position, Quaternion.identity);
+            Rigidbody2D prb = proj.GetComponent<Rigidbody2D>();
+            if (prb != null)
+                prb.linearVelocity = moveInput.normalized * 10f;
+        }
+
+        Invoke(nameof(ResetAttack), attackCooldown);
+    }
+
+    private void ResetAttack() => canAttack = true;
+}
